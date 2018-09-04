d53b6f1a2777fc5303d4c4822f7976dbf83b8150
Nicholas Chun Markus <nicxzmiller2@gmail.com>
2018-09-04 02:35:49 +0200

    first commit,Landing Page

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Index from "./components/layouts/Index";
+import { Provider } from "./context";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <Router>
+          <React.Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Index} />
+              </Switch>
+            </div>
+          </React.Fragment>
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="nabvar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 h1">Lyric Catch</span>
+    </div>
+  );
+};
+
+export default Navbar;
